8770d145080a740f848c6ab4dc93de11664b2c4f
chriszhu <1633711653@qq.com>
2020-12-29 17:10:35 +0800

    chat rebuild

--- chat/_assets/index.3a24c4a9.js
+++ chat/_assets/index.3a24c4a9.js
@@ -1,4 +1,4 @@
-import{d as t,m as e,n as o,q as i,o as n,c as s,k as r}from"./index.a91396de.js";
+import{d as t,m as e,n as o,q as i,o as n,c as s,k as r}from"./index.6bcfca21.js";
 /*!
  * better-scroll / core
  * (c) 2016-2020 ustbhuangyi
